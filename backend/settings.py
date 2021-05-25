e815aa9a7f61007631113f35ba7434e92db3bd7e
Niall O Hara <nohara@renttherunway.com>
2021-05-25 13:25:54 +0100

    added aws region name

--- backend/settings.py
+++ backend/settings.py
@@ -198,13 +198,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID'),
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY'),
-AWS_ACCESS_KEY_ID = 'AKIAX4LAPPMOWN73APEV',
-AWS_SECRET_ACCESS_KEY = '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',
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID'),
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY'),
 AWS_STORAGE_BUCKET_NAME = 'niall-proshop'
 AWS_S3_SIGNATURE_VERSION = 's3'
 AWS_QUERYSTRING_AUTH = False
+AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_S3_CUSTOM_DOMAIN = 'niall-proshop.s3.amazonaws.com'
 
 if os.getcwd() == '/app':
     DEBUG = False
